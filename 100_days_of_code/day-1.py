31329ec40fb7406f033a47526307563bb2fe7470
Lukanny <vinicius_loureiro_cavalcante@hotmail.com>
2023-09-04 12:36:09 -0400

    Starting 100 Days of Code course's projects.

---
+++ 100_days_of_code/day-1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name generator!")
+city = input("What city were you born?\n")
+pet_name = input("What was the name of your first pet?\n")
+band_name = city + " " + pet_name
+print(band_name)
